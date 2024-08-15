edaa58143d0e6a827cbbf5ded2154b168f511b9a
DF <dmytro.furmanenko@gmail.com>
2024-08-15 19:50:44 +0300

    update index.html styles

---
+++ src/aqa_lection5_warmup/NumberChecker.java
@@ -0,0 +1,22 @@
+package aqa_lection5_warmup;
+
+public class NumberChecker {
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPrime(0));
+        System.out.println(isPrime(8));
+        System.out.println(isPrime(7));
+    }
+}
+
