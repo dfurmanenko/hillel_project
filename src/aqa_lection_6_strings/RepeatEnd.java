44042ff5c2c94d2f511241557df7a40c443c3325
DF <dmytro.furmanenko@gmail.com>
2024-08-18 17:26:22 +0300

    update index.html styles

--- src/aqa_lection_6_strings/RepeatEnd.java
+++ src/aqa_lection_6_strings/RepeatEnd.java
@@ -2,14 +2,14 @@ package aqa_lection_6_strings;
 
 
 public class RepeatEnd {
+    public static String repeatEnd(String str, int n) {
+        String end = str.substring(str.length() - n);
+        return end.repeat(n);
+    }
+
     public static void main(String[] args) {
         System.out.println(repeatEnd("Hello", 3));
         System.out.println(repeatEnd("Hello", 2));
         System.out.println(repeatEnd("Hello", 1));
-    }
-
-    public static String repeatEnd(String str, int n) {
-        String end = str.substring(str.length() - n);
-        return end.repeat(n);
     }
 }
