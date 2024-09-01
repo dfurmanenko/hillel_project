fa0a2d38b72d862ebd519d7a772914c2e9958288
DF <dmytro.furmanenko@gmail.com>
2024-09-01 21:49:38 +0300

    update index.html styles

---
+++ src/aqa_lection9_collections/CityCodesExample.java
@@ -0,0 +1,23 @@
+package aqa_lection9_collections;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CityCodesExample {
+    public static void main(String[] args) {
+
+        Map<String, String> cityCodes = new HashMap<>();
+
+        // Додаємо міста та їхні телефонні коди до мапи
+        cityCodes.put("Київ", "044");
+        cityCodes.put("Харків", "057");
+        cityCodes.put("Одеса", "048");
+        cityCodes.put("Львів", "032");
+        cityCodes.put("Дніпро", "056");
+
+
+        for (Map.Entry<String, String> entry : cityCodes.entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue());
+        }
+    }
+}
